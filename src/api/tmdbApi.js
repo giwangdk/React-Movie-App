db8b125454f18034bd2f840469fd205a7bdec1c7
gidwikintan <gidwikintan@gmail.com>
2021-11-11 16:07:20 +0700

    config API

--- src/api/tmdbApi.js
+++ src/api/tmdbApi.js
@@ -17,3 +17,38 @@ export const tvType = {
     'on_the-air': 'on_the_air'
 };
 
+const tmdbApi = {
+    getMovieList: (type, params) => {
+        const url = 'movie/' + movieType[type];
+        return axiosClient.get(url, params)
+    },
+    getTvList: (type, params) => {
+        const url = 'tv/' + tvType[type];
+        return axiosClient.get(url, params)
+    },
+    getVideos: (cate,id) => {
+        const url = category[cate] + '/' + id + '/videos';
+        return axiosClient.get(url, {params: {}})
+    },
+    search: (cate,params) => {
+        const url =  'search/' + category[cate];
+        return axiosClient.get(url, params)
+    },
+    detail: (cate, id, params) => {
+        const url = category[cate] + '/' +  id;
+        return axiosClient.get(url, params)
+    },
+    credits: (cate, id, params) => {
+        const url = category[cate] + '/' +  id + '/credits';
+        return axiosClient.get(url, params)
+    },
+    similar: (cate, id, params) => {
+        const url = category[cate] + '/' +  id + '/similar';
+        return axiosClient.get(url, params)
+    }
+}
+
+export default tmdbApi;
+
+
+
