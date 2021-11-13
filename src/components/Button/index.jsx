df48677a293c281c60984f012b64c6f53fc31bff
gidwikintan <gidwikintan@gmail.com>
2021-11-13 13:41:25 +0700

    crate button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Button = props => {
+    return (
+        <button className={`btn ${props.className}`} onClick={props.onClick ? () => props.onClick() : null}>
+            {props.children}
+        </button>
+    )
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default Button
